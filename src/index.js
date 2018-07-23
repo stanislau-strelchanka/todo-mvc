febb47f6000ec8a8ce4a964c1fa2053029730354
stashewski <stanislav.strelchenko@banuba.com>
2018-07-23 14:49:07 +0300

    initiall commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<dev><h1>To do list</h1></dev>, document.getElementById('root'));
+registerServiceWorker();
