949042d684e4bbe7cb37edfa1bb6af4332d27ed3
stashewski <stanislav.strelchenko@banuba.com>
2018-07-23 16:23:34 +0300

    create static todo list

--- src/index.js
+++ src/index.js
@@ -3,5 +3,56 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<dev><h1>To do list</h1></dev>, document.getElementById('root'));
+function TodoList(props) {
+    let todos = [1, 2, 3].map((item) => {
+        return <TodoItem value={item} isChecked={item % 2 === 0}/>;
+    });
+
+    return (
+        <div>
+            <h1>To do list</h1>
+            <AddTodo/>
+            <ul>{todos}</ul>
+            <TodoFilter/>
+        </div>
+    )
+}
+
+function AddTodo(props) {
+    return (
+        <div>
+            <input type="text"/>
+            <input type="button" value="Add Todo"/>
+        </div>
+    );
+}
+
+function TodoItem(props) {
+    return (
+        <li>
+            <label>
+                {props.value}
+                <input type="checkbox" checked={props.isChecked} />
+            </label>
+        </li>
+    );
+}
+
+function TodoFilter(props) {
+    return (
+        <div>
+            <label>
+                <input type="radio" name="filter" value="All" defaultChecked /> All
+            </label>
+            <label>
+                <input type="radio" name="filter" value="Active"/> Active
+            </label>
+            <label>
+                <input type="radio" name="filter" value="Completed"/> Completed
+            </label>
+        </div>
+    );
+}
+
+ReactDOM.render(<TodoList/>, document.getElementById('root'));
 registerServiceWorker();
